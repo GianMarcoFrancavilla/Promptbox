7fb28526fa12b5af02ed55491c15e51ebf6e959b
Gian Marco Francavilla <114476866+GianMarcoFrancavilla@users.noreply.github.com>
2025-11-08 15:59:26 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['@supabase/supabase-js']
+    }
+  },
+  optimizeDeps: {
+    exclude: ['@supabase/supabase-js']
+  }
 })

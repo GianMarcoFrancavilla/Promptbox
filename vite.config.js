6083f164ac2cf8d5009c78421e1d09bdf7e5cce9
Gian Marco Francavilla <114476866+GianMarcoFrancavilla@users.noreply.github.com>
2025-11-16 11:06:47 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Promptbox/', // Aggiungi questa linea
   build: {
     rollupOptions: {
       external: ['@supabase/supabase-js']
